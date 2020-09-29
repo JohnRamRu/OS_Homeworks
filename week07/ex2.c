46691a2786048af60d77be3b7f03f44c963b6c5a
Evgeny Ageev <johnramru@gmail.com>
2020-09-29 17:03:20 +0300

    Update ex2.c

--- week07/ex2.c
+++ week07/ex2.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 int main()
 {
     int N;
 
     scanf("%d", N);
+
     int *values = malloc(N * sizeof(int));
+
+    print(sizeof(values));
 
     return 0;
 }
